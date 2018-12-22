8769090e5ab3b7107953f3e1874a2381ba2916c3
Wes Hardaker <opensource@hardakers.net>
2018-12-22 08:22:11 -0800

    Yes, I actually wrote a test suite

---
+++ test.py
@@ -0,0 +1,22 @@
+import unittest
+
+class CWTests(unittest.TestCase):
+    def test_hhmm(self):
+        import starterbot as s
+        
+        self.assertEqual(s.sec_to_hhmm(30), "00:30")
+        self.assertEqual(s.sec_to_hhmm(59), "00:59")
+        self.assertEqual(s.sec_to_hhmm(60), "01:00")
+        self.assertEqual(s.sec_to_hhmm(61), "01:01")
+        self.assertEqual(s.sec_to_hhmm(121), "02:01")
+
+    def test_average(self):
+        import starterbot as s
+
+        scores=[{'date': "12/10", "time": 50}]
+
+        self.assertEqual(s.average_score({}), 0.0)
+        self.assertEqual(s.average_score(scores), 50)
+
+        scores.append({'date': "12/11", "time": 150})
+        self.assertEqual(s.average_score(scores), 100)
