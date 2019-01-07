b2e300cc0e7f7274be1872dcc168ffb7e390c333
Wes Hardaker <opensource@hardakers.net>
2019-01-07 08:23:36 -0800

    test for printing sorted scores

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import unittest
 
+def mock_find_user(user):
+    if user == 'user1':
+        return {'real_name': "user one"}
+    return {'real_name': "user two"}
+
 class CWTests(unittest.TestCase):
     def test_hhmm(self):
         import cwbot as s
@@ -24,3 +29,25 @@ class CWTests(unittest.TestCase):
     # def test_date(self):
     #     import cwbot as s
     #     self.assertEqual(s.make_datestr(), "2018/12/22")
+
+    def test_sorted_averages(self):
+        import cwbot as s
+        import sys
+
+        data={'user1': {'times': [{'time': 30}]},
+              'user2': {'times': [{'time': 40}]},
+        }
+        s.our_data = {'cwtimes': data}
+        s.find_user = mock_find_user
+
+        results = s.bot_score("test", "user1", {}, {})
+
+        self.assertTrue(results.find('user one') < results.find('user two'),
+                        "user one is faster")
+
+        data['user1']['times'].append({'time': 1000})
+         
+        results = s.bot_score("test", "user1", {}, {})
+        sys.stderr.write(results)
+        self.assertTrue(results.find('user one') > results.find('user two'),
+                        "user one is slower")
